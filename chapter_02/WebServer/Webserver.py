16a8dfe9b54fa47ebffc29290431854cd2b0eb2d
ngxxx307 <ngxxx307@umn.edu>
2025-02-23 14:45:51 +0800

    Add chapter2 web server programming assignment

--- chapter_02/WebServer/Webserver.py
+++ chapter_02/WebServer/Webserver.py
@@ -1,4 +1,5 @@
 from socket import socket, AF_INET, SOCK_STREAM
+import json
 from enum import Enum
 
 
@@ -6,9 +7,26 @@ serverPort = 12000
 
 httpMethods = {"GET"}
 
+line_separator = "\r\n"
+
+def format_http_response(status_line: str, headers: dict = None, body: bytes = b"") -> bytes:
+    # Default headers dictionary if none provided
+    headers = headers or {}
+
+    if body:
+        headers["Content-Length"] = len(body)
+
+    headers_formatted = "\r\n".join(f"{key}: {value}" for key, value in headers.items())
+
+    response = (
+        f"{status_line}\r\n" 
+        f"{headers_formatted}\r\n\r\n"
+    ).encode() + body 
+
+    return response
+
 def parseHTTP(msg: str):
-    
-    msgList = msg.split("\r\n")
+    msgList = msg.split(line_separator)
     
     try:
         method, path, httpVersion = msgList[0].split(" ")
@@ -16,9 +34,9 @@ def parseHTTP(msg: str):
         headers = msgList[1:-1]
 
         if httpVersion != "HTTP/1.1":
-            return "HTTP/1.1 400 Bad Request\r\n\r\n"
+            return format_http_response("HTTP/1.1 400 Bad Request")
         if method not in httpMethods:
-            return "HTTP/1.1 405 Method Not Allowed\r\n\r\n"
+            return format_http_response("HTTP/1.1 405 Method Not Allowed")
         
         filepath = ""
         stripped_path = path.lstrip("/")
@@ -26,31 +44,27 @@ def parseHTTP(msg: str):
             filepath = "index.html"
         else:
             filepath = stripped_path
-        print("filepath:", filepath, path)
         
         with open(filepath, mode='rb') as f:
             content = f.read()
-        print(content)
+        content_type = getContentType(filepath.split(".")[1])
+
+        return format_http_response("HTTP/1.1 400 Bad Request", {"Content-Type": content_type, "Connection": "close"}, content)
     except FileNotFoundError:
-        return  "HTTP/1.1 404 Not Found\r\n\r\n"
-    except Exception as e:
-        print(e)
-        print(type(e))
-        return "HTTP/1.1 400 Bad Request\r\n\r\n"
-    return f"HTTP/1.1 200 OK\r\n\r\n"
+        return format_http_response("HTTP/1.1 404 Not Found")
 
 def getContentType(fileExtension: str) -> str:
     # Map file extensions to Content-Type
     contentTypes = {
-        ".html": "text/html",
-        ".css": "text/css",
-        ".js": "application/javascript",
-        ".json": "application/json",
-        ".png": "image/png",
-        ".jpg": "image/jpeg",
-        ".jpeg": "image/jpeg",
-        ".gif": "image/gif",
-        ".txt": "text/plain",
+        "html": "text/html",
+        "css": "text/css",
+        "js": "application/javascript",
+        "json": "application/json",
+        "png": "image/png",
+        "jpg": "image/jpeg",
+        "jpeg": "image/jpeg",
+        "gif": "image/gif",
+        "txt": "text/plain",
     }
     return contentTypes.get(fileExtension, "application/octet-stream")  # Default
 
@@ -58,12 +72,11 @@ def getContentType(fileExtension: str) -> str:
 with socket(AF_INET, SOCK_STREAM) as serverSocket:
     serverSocket.bind(('', serverPort))
     serverSocket.listen(1)  # Max number of pending connections in the queue
-    print('The server is ready to receive')
+    print(f'The server is listen on port {serverPort}')
 
     while True:
         # Accept a new connection within the context of the server socket
         with serverSocket.accept()[0] as connSocket:
             msg = connSocket.recv(1024).decode()
             resp = parseHTTP(msg)
-            print("resp: ", resp)
-            connSocket.send(resp.encode())
+            connSocket.send(resp)
